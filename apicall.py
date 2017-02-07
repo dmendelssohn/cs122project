a7d1abd83bccde033b27e0bb0bbaad81aa29e9b4
dmendelssohn <dmendelssohn@uchicago.edu>
2017-02-07 17:42:18 -0600

    apicall works

---
+++ apicall.py
@@ -0,0 +1,19 @@
+import hashlib
+import requests
+import time
+
+name = arg
+
+m = hashlib.md5()
+pub = "a86a6cb4e0255630ae1eae723b451328"
+priv = "fae88800e9cd608f2dcf1eda19e58e93514aad78"
+t = time.time()
+m.update((str(t)+priv + pub).encode('utf-8'))
+hashed = m.hexdigest()
+params = {"hash":hashed,"apikey":pub,"ts":t,"name":name}
+url = "https://gateway.marvel.com/v1/public/characters"
+req = requests.get(url,params=params)
+if req.json()["code"] == 200:
+    desc = req.json()["data"]["results"][0]["description"]
+else:
+    print("sorry, idiot")
