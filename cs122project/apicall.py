c6ea9e7cf55161b964cec5de3f2b2dba8a6e8d1d
dmendelssohn <dmendelssohn@uchicago.edu>
2017-02-09 17:22:17 -0600

    private apicall

---
+++ cs122project/apicall.py
@@ -0,0 +1,23 @@
+import hashlib
+import requests
+import time
+
+name = arg
+
+m = hashlib.md5()
+pub = "a86a6cb4e0255630ae1eae723b451328"
+priv = ""
+t = time.time()
+m.update((str(t)+priv + pub).encode('utf-8'))
+hashed = m.hexdigest()
+params = {"hash":hashed,"apikey":pub,"ts":t,"name":name}
+url = "https://gateway.marvel.com/v1/public/characters"
+req = requests.get(url,params=params)
+if req.json()["code"] == 200:
+    desc = req.json()["data"]["results"][0]["description"]
+else:
+    print("sorry, idiot")
+
+# images get request
+img = requests.get(req.json()['data']['results'][0]['thumbnail']['path'] + 
+    'detail' + req.json()['data']['results'][0]['thumbnail']['extension'])
