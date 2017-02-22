da0a4c1c8101aaa2febaf7b81e98f1566c26d7a5
Anne Rogers <amr@cs.uchicago.edu>
2017-02-22 16:59:12 -0600

    sql query case insensitive, returns all col values

--- ui/fivethirtyeight.py
+++ ui/fivethirtyeight.py
@@ -7,27 +7,28 @@ import os
 DATA_DIR = os.path.dirname(__file__)
 DATABASE_FILENAME = os.path.join(DATA_DIR, 'fivethirtyeight_marvel.db')
 
-d1 = {'name': ['name'],
-      'ID': ['name', 'ID'],
-      'ALIGN': ['name', 'ALIGN'],
-      'EYE': ['name', 'EYE'],
-      'HAIR': ['name', 'HAIR'],
-      'SEX': ['name', 'SEX'],
-      'ALIVE': ['name', 'ALIVE'],
-      'APPEARANCES': ['name', 'APPEARANCES'],
-      'FIRST APPEARANCES': ['name', 'FIRST APPEARANCES'],
-      'YEAR': ['name', 'YEAR']}
+d1 = {'name': ['name', 'id', 'align', 'eye', 'hair', 'sex', 'alive',
+               'appearances', 'first_appearance', 'year'],
+      'id': ['name', 'id'],
+      'align': ['name', 'align'],
+      'eye': ['name', 'eye'],
+      'hair': ['name', 'hair'],
+      'sex': ['name', 'sex'],
+      'alive': ['name', 'alive'],
+      'appearances': ['name', 'appearances'],
+      'first_appearance': ['name', 'first_appearance'],
+      'year': ['name', 'year']}
 
 d2 = {'name': (1, 'name'),
-      'ID': (2, 'ID'),
-      'ALIGN': (3, 'ALIGN'),
-      'EYE': (4, 'EYE'),
-      'HAIR': (5, 'HAIR'),
-      'SEX': (6, 'SEX'),
-      'ALIVE': (7, 'ALIVE'),
-      'APPEARANCES': (8, 'APPEARANCES'),
-      'FIRST APPEARANCES': (9, 'FIRST APPEARANCES'),
-      'YEAR': (10, 'YEAR')}
+      'id': (2, 'id'),
+      'align': (3, 'align'),
+      'eye': (4, 'eye'),
+      'hair': (5, 'hair'),
+      'sex': (6, 'sex'),
+      'alive': (7, 'alive'),
+      'appearances': (8, 'appearances'),
+      'first_appearance': (9, 'first_appearance'),
+      'year': (10, 'year')}
 
 def determine_attributes(args_from_ui):
     '''
@@ -97,16 +98,16 @@ def find_attributes(args_from_ui):
     marvel character information that match the criteria.  The dictionary
     will contain some of the following fields:
 
-      - name (str) e.g. Spider-Man (Peter Parker)
-      - ID (str) e.g. 'Secret Identity' or 'Public Identity'
-      - Align (str) e.g 'Good Characters' or 'Neutral Characters'
-      - Eye (str) e.g. 'Hazel Eyes'
-      - Hair (str) e.g. 'Brown Hair'
-      - Sex (str) e.g. 'Male Characters'
-      - Alive (str) e.g. 'Alive Characters'
-      - Appearances (int) e.g. 4043
-      - First Appearance (str) e.g. 'Oct-74'
-      - Year (int) e.g. 1941
+      - name (str) e.g. S'ider-Man (Peter Parker)
+      -'id (str) e.g. 'Secret'identity' or 'Public'identity'
+      - align (str) e.g 'Good Characters' or 'Neutral Characters'
+      - eye (str) e.g. 'Hazel eyes'
+      - hair (str) e.g. 'Brown hair'
+      - sex (str) e.g. 'Male Characters'
+      - alive (str) e.g. 'alive Characters'
+      - appearances (int) e.g. 4043
+      - first_appearance (str) e.g. 'Oct-74'
+      - year (int) e.g. 1941
     '''
 
     # replace with a list of the attribute names in order and a list
@@ -123,7 +124,7 @@ def find_attributes(args_from_ui):
     connection = sqlite3.connect(DATABASE_FILENAME)
     cursor = connection.cursor()
 
-    S = 'SELECT ' + attribute_string + ' FROM marvel' + where_clause 
+    S = 'SELECT ' + attribute_string + ' FROM marvel' + where_clause + ' COLLATE NOCASE'
     query = cursor.execute(S, params)     
 
     result = query.fetchall()
