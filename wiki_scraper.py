b6f0854e6fa788e254d3a33c4e9ce8bd5f9c1e69
Justin Klein <jmklein@cs.uchicago.edu>
2017-02-23 12:14:56 -0600

    added case where there is no ability table to scraper

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -6,7 +6,12 @@ import requests
 r = requests.get('http://en.wikipedia.org/wiki/%s' % (arg))
 soup = bs4.BeautifulSoup(r.text)
 ths = soup.find_all('th')
-abilities_list = [li.text for li in [th.next_sibling.next_sibling.find_all("li") for th in ths if th.text == "Abilities"][0]]
+lis = [th.next_sibling.next_sibling.find_all("li") for th in ths if th.text == "Abilities"]
+if lis != []:
+    abilities_list = [li.text for li in lis[0]]
+else:
+    abilities_list = ['No specific abilities']
+
 if soup.find_all('a', class_='mw-redirect', title=arg) == 0:
     alter_ego = soup.find_all(
         'th', text='Alter ego')[0].next_sibling.next_sibling.text
