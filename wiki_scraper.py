eb63b5a426b4a981752da6b29acca91032dd2682
Justin Klein <jmklein@cs.uchicago.edu>
2017-02-22 18:53:13 -0600

    add alter_ego to scraper

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,4 +1,3 @@
-import re
 import bs4
 import json
 import sys
@@ -8,4 +7,8 @@ r = requests.get('http://en.wikipedia.org/wiki/%s' % (arg))
 soup = bs4.BeautifulSoup(r.text)
 ths = soup.find_all('th')
 abilities_list = [li.text for li in [th.next_sibling.next_sibling.find_all("li") for th in ths if th.text == "Abilities"][0]]
-alter_ego = 
+if soup.find_all('a', class_='mw-redirect', title=arg) == 0:
+    alter_ego = soup.find_all(
+        'th', text='Alter ego')[0].next_sibling.next_sibling.text
+else:
+    alter_ego = ths[0].text
