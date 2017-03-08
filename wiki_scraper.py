43742ab98961183f512d19c7b60f038dfe3dfbc6
Justin Klein <jmklein@cs.uchicago.edu>
2017-03-08 17:53:46 -0600

    functionized, returns dictionary

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -3,63 +3,77 @@ import json
 import sys
 import requests
 
-r = requests.get('http://en.wikipedia.org/wiki/%s' % (arg))
-soup = bs4.BeautifulSoup(r.text)
-ths = soup.find_all('th')
 
-ab_lis = [th.next_sibling.next_sibling.find_all('li') for th in ths if th.text == 'Abilities']
-if ab_lis != []:
-    abilities_list = [li.text for li in ab_lis[0]]
-else:
-    abilities_list = ['No specific abilities']
+def scraper(arg):
+    info_dict = {}
+    r = requests.get('http://en.wikipedia.org/wiki/%s' % (arg))
+    soup = bs4.BeautifulSoup(r.text)
+    ths = soup.find_all('th')
 
-ta_lis = [th.next_sibling.next_sibling.find_all('li') for th in ths if th.text == 'Team affiliations']
-if ta_lis != []:
-    team_list = [li.text for li in ta_lis[0]]
-else:
-    team_list = ['No team affiliations']
+    ab_lis = [th.next_sibling.next_sibling.find_all('li') for th in ths if th.text == 'Abilities']
+    if ab_lis != []:
+        abilities_list = [li.text for li in ab_lis[0]]
+    else:
+        abilities_list = ['No specific abilities']
+    info_dict['Abilities'] = abilities_list
 
-sp_lis = [th.next_sibling.next_sibling.find_all('li') for th in ths if th.text == 'Species']
-if sp_lis != []:
-    species_list = [li.text for li in sp_lis[0]]
-else:
-    species_list = ['No known species']
+    ta_lis = [th.next_sibling.next_sibling.find_all('li') for th in ths if th.text == 'Team affiliations']
+    if ta_lis != []:
+        team_list = [li.text for li in ta_lis[0]]
+    else:
+        team_list = ['No team affiliations']
+    info_dict['Teams'] = team_list
 
-publisher_list = ['No publisher information available']
-for th in ths[:20]:
-    if th.text == 'Publisher':
-        publisher_list = [th.next_sibling.next_sibling.text]
+    sp_lis = [th.next_sibling.next_sibling.find_all('li') for th in ths if th.text == 'Species']
+    if sp_lis != []:
+        species_list = [li.text for li in sp_lis[0]]
+    else:
+        species_list = ['No known species']
+    info_dict['Species'] = species_list
 
-first_appearance_list = ['No first appearance information available']
-for th in ths[:20]:
-    if th.text == 'First appearance':
-        first_appearance_list = [th.next_sibling.next_sibling.text]
+    publisher_list = ['No publisher information available']
+    for th in ths[:20]:
+        if th.text == 'Publisher':
+            publisher_list = [th.next_sibling.next_sibling.text]
+    info_dict['Publisher'] = publisher_list
 
-cr_as = [th.next_sibling.next_sibling.find_all('a') for th in ths if th.text == 'Created by']
-if cr_as != []:
-    creator_list = [a.text for a in cr_as[0]]
-else:
-    creator_list = ['No creator information available']
+    first_appearance_list = ['No first appearance information available']
+    for th in ths[:20]:
+        if th.text == 'First appearance':
+            first_appearance_list = [th.next_sibling.next_sibling.text]
+    info_dict['First appearance'] = first_appearance_list
 
-partnership_list = ['No partnership information available']
-for th in ths[:20]:
-    if th.text == 'Partnerships':
-        partnership_list = [a.text for a in th.next_sibling.next_sibling.find_all('a')]
+    cr_as = [th.next_sibling.next_sibling.find_all('a') for th in ths if th.text == 'Created by']
+    if cr_as != []:
+        creator_list = [a.text for a in cr_as[0]]
+    else:
+        creator_list = ['No creator information available']
+    info_dict['Creators'] = creator_list
 
-aliases_list = ['No alias information available']
-for th in ths[:20]:
-    if th.text == 'Notable aliases':
-        aliases_list = [li.text[:-3] for li in th.next_sibling.next_sibling.find_all('li')]
+    partnership_list = ['No partnership information available']
+    for th in ths[:20]:
+        if th.text == 'Partnerships':
+            partnership_list = [a.text for a in th.next_sibling.next_sibling.find_all('a')]
+    info_dict['Partnerships'] = partnership_list
 
-if soup.find_all('a', class_='mw-redirect', title=arg) == 0:
-    alter_ego = soup.find_all(
-        'th', text='Alter ego')[0].next_sibling.next_sibling.text
-else:
-    alter_ego = ths[0].text
+    aliases_list = ['No alias information available']
+    for th in ths[:20]:
+        if th.text == 'Notable aliases':
+            aliases_list = [li.text[:-3] for li in th.next_sibling.next_sibling.find_all('li')]
+    info_dict['Aliases'] = aliases_list
+
+    if soup.find_all('a', class_='mw-redirect', title=arg) == 0:
+        alter_ego = soup.find_all(
+            'th', text='Alter ego')[0].next_sibling.next_sibling.text
+    else:
+        alter_ego = ths[0].text
+    info_dict['Alter ego'] = alter_ego
+
+    return info_dict
 
 #Finding dopplegangers, probably belongs in another file
-df = pd.read_csv('./fivethirtyeight_marvel.csv')
-df2 = pd.read_csv('./marvel_sans_earth.csv')
-joined = pd.concat([df, df2['name_no_earth']], axis=1)
-test = joined['name_no_earth'].str.extract(
-    r'^([^\(]*)(?: \((.*)\))*$', expand=True)
+#df = pd.read_csv('./fivethirtyeight_marvel.csv')
+#df2 = pd.read_csv('./marvel_sans_earth.csv')
+#joined = pd.concat([df, df2['name_no_earth']], axis=1)
+#test = joined['name_no_earth'].str.extract(
+#    r'^([^\(]*)(?: \((.*)\))*$', expand=True)
