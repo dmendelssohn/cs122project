1eb4f8c652f620a21c0fdf17a6f168ba9cb70c06
Justin Klein <jmklein@cs.uchicago.edu>
2017-03-08 16:35:37 -0600

    turned into function, finished image call

--- apicall.py
+++ apicall.py
@@ -4,28 +4,31 @@ import time
 from priv_key import priv
 import csv
 
-name = arg
 
-m = hashlib.md5()
-pub = "a86a6cb4e0255630ae1eae723b451328"
-#Not-working private key extraction from home directory
-'''
-with open('../priv_key.csv', 'w') as keyfile:
-    keyreader = csv.reader(keyfile)
-    for row in keyreader:
-        priv = row
-'''
-t = time.time()
-m.update((str(t) + priv + pub).encode('utf-8'))
-hashed = m.hexdigest()
-params = {"hash":hashed,"apikey":pub,"ts":t,"name":name}
-url = "https://gateway.marvel.com/v1/public/characters"
-req = requests.get(url,params=params)
-if req.json()["code"] == 200:
-    desc = req.json()["data"]["results"][0]["description"]
-else:
-    print("sorry, idiot")
+def apicall(name):
 
-# images get request
-img = requests.get(req.json()['data']['results'][0]['thumbnail']['path'] + 
-    '/detail.' + req.json()['data']['results'][0]['thumbnail']['extension'])
+    m = hashlib.md5()
+    pub = "a86a6cb4e0255630ae1eae723b451328"
+
+    desc = ''
+    t = time.time()
+    m.update((str(t) + priv + pub).encode('utf-8'))
+    hashed = m.hexdigest()
+    params = {"hash":hashed,"apikey":pub,"ts":t,"name":name}
+    url = "https://gateway.marvel.com/v1/public/characters"
+    req = requests.get(url,params=params)
+    if req.json()["code"] == 200:
+        desc += req.json()["data"]["results"][0]["description"]
+
+    img = requests.get(req.json()['data']['results'][0]['thumbnail']['path'] + 
+        '/detail.' + req.json()['data']['results'][0]['thumbnail']['extension'],
+        stream=True)
+    if img.status_code == 200:
+        with open('character_image.jpg', 'wb') as writer:
+            for piece in img:
+                writer.write(piece)
+
+    if desc == '':
+        return 'Sorry, no character description available'
+    else:
+        return desc 
