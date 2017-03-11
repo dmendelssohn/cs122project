fb5ba9c02ea3829d182cc74dfc647cb73ea73663
dmendelssohn <dmendelssohn@uchicago.edu>
2017-03-10 23:46:36 -0600

    Cleaned graphing data, edit for too large graphs

--- grapher.py
+++ grapher.py
@@ -1,10 +1,12 @@
 import networkx as nx
 from matplotlib import pyplot as plt
-G = nx.read_pajek('hero_network.txt')
+G = nx.read_pajek('clean_hero_network.txt')
 
 def get_network(arg):
     plt.clf()
-    N = G.neighbors(arg.upper())
+    arg = arg.upper()
+    N = G.neighbors(arg)
+    N.append(arg)
     H = G.subgraph(N)
     nx.draw_circular(H,with_labels=True,alpha=0.2,edge_color='0.75')
     plt.show()
