f71490921834bafc14c1208aefd37c57c8ee5a30
dmendelssohn <dmendelssohn@uchicago.edu>
2017-03-12 16:44:11 -0500

    added most links/num links

--- grapher.py
+++ grapher.py
@@ -4,13 +4,16 @@ G = nx.read_pajek('weighted_hero_network.txt')
 
 def get_network(arg):
     plt.clf()
+    plt.figure(figsize=(10,8))
     arg = arg.upper()
     N = G.neighbors(arg)
     N.append(arg)
     H = G.subgraph(N)
     d = nx.degree(H)
+    weights = H[arg]
+    most_apps = sorted([(x,weights[x][0]['weight']) for x in weights], key=lambda i: i[1])[-1][0]
     nx.draw_circular(H,with_labels=True,alpha=0.5,edge_color='0.5',
         nodelist=d.keys(), node_size=[v * 5 for v in d.values()])
-    plt.show()
-    plt.savefig("network.pdf")
+    plt.savefig("network.jpg")
     plt.clf()
+    return (most_apps,len(N))
