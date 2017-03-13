1f8f3501c7ae95d7834fc759ad9115c81479d53f
dmendelssohn <dmendelssohn@uchicago.edu>
2017-03-12 23:19:10 -0500

    finished marvel cleaner

--- marvel_cleaner.py
+++ marvel_cleaner.py
@@ -2,7 +2,22 @@ import pandas as pd
 #we had to deal with unicode encodings (ie \u00e9) manually because
 #slashes were not written as escape characters
 
-marveldf = pd.read_csv('marvel_sans_earth.csv')
+marveldf = pd.read_csv('marvel-wikia-data.csv')
+suffs = marveldf['name'].str.extract(r'^[^(]+(?:\([^\)]+\))?(?:(\([^\)]*Earth[^\)]*\)))',
+    expand=True)
+
+suffs = list(set(suffs[0]))
+suffs = suffs[1:]
+suffs = [" " + x for x in suffs]
+
+def earth_replace(s,suff):
+    return s.replace(suff,"")
+
+marveldf["name_no_earth"] = marveldf["name"]
+
+for suff in suffs:
+    marveldf["name_no_earth"] = marveldf["name_no_earth"].apply(lambda row: earth_replace(row,suff))
+
 identifiers = marveldf['name_no_earth'].str.extract(r'^([^(]+)(?:\(([^\)]+)\))*',
     expand=True)
 identifiers.columns = ["hero_name","alias"]
@@ -10,19 +25,3 @@ fulldf = pd.concat([marveldf,identifiers["hero_name"].str.strip(),
     identifiers["alias"].str.strip()],axis=1)
 
 fulldf.to_csv('marvel_split_names.csv',index=False)
-
-
-rawdf = pd.read_csv('marvel-wikia-data.csv')
-suffs = rawdf['name'].str.extract(r'^[^(]+(?:\([^\)]+\))?(?:\(([^\)]*Earth[^\)]*)\))',
-    expand=True)
-
-suffs = list(set(suffs[0]))
-suffs = suffs[1:]
-suffs = r"(" + r")|(".join(suffs) + r")"
-
-suffs = r'\(Earth-61018\)|\(Earth-148611\)|\(Earth-9510\)|\(Earth-88194\)|' + \
-    r'\(Earth-TRN361\)|\(Earth-TRN259\)|\(Earth-20476\)|\(Earth-41001\)|' + \
-    r'\(Earth-11052\)|\(Earth-982\)|\(Earth-4096\)|\(Earth-5311\)|\(Earth-616\)|' + \
-    r'\(Earth-20051)|(Earth-11418)|(Earth-98121)|(Earth-5106)|(Earth-941066)|(Earth-8410)|(Sub-Earth Men)|(Earth-1610)|(Earth-5012)|(Counter-Earth)'
-
-name_no_earth = rawdf['name'].str.extract(r'^([.*]+) (?:' + suffs + ')')
