be9613999070c75f36a90235ca58ce815c0697b7
Anne Rogers <amr@cs.uchicago.edu>
2017-03-12 19:00:25 -0500

    changed direcotry for network files, grapher

--- grapher.py
+++ grapher.py
@@ -1,6 +1,9 @@
 import networkx as nx
 from matplotlib import pyplot as plt
+import os 
 G = nx.read_pajek('weighted_hero_network.txt')
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def get_color(arg,name):
     if name == arg:
@@ -39,12 +42,12 @@ def get_network(arg,conn_num=False):
             nx.draw_circular(lim_H,with_labels=True,alpha=0.5,edge_color='0.5',
                 nodelist=lim_d.keys(), node_size=[weights[v][0]['weight'] * 5 for v in lim_d.keys()],
                 node_color=[get_color(arg,v) for v in lim_d.keys()])
-            plt.savefig("lim_network.jpg")
+            plt.savefig(os.path.join(BASE_DIR,'ui/static/lim_network.jpg')
             plt.clf()
 
         nx.draw_circular(H,with_labels=True,alpha=0.5,edge_color='0.5',
             nodelist=d.keys(), node_size=[weights[v][0]['weight'] * 5 for v in d.keys()],
             node_color=[get_color(arg,v) for v in d.keys()])
-        plt.savefig("network.jpg")
+        plt.savefig(os.path.join(BASE_DIR,'ui/static/network.jpg'))
         plt.clf()
         return (1,most,len(N),limit)
